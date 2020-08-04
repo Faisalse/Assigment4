8ab32358cce590dbf3adbe7d836efbe673922f5c
Faisalse <faisalaslam156@gmail.com>
2020-08-04 00:23:05 -0700

    some changes

---
+++ Project/Assets/splash/sp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sp : MonoBehaviour
+{
+    int ScreenNumber;
+    public void start()
+    {
+        ScreenNumber = 1;
+        SceneManager.LoadScene(ScreenNumber);
+    }
+
+    public void returnn()
+    {
+        ScreenNumber = 0;
+        SceneManager.LoadScene(ScreenNumber);
+    }
+}
